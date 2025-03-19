76e719ab23779e4d63240a98663de4172d01e059
jakobpederson <jakob.pederson+github@gmail.com>
2025-03-19 08:48:40 -0700

    update actions, engine

--- actions.py
+++ actions.py
@@ -1,14 +1,26 @@
 class Action:
-    pass
+    def perform(self, engine, entity):
+        raise NotImplementedError()
 
 
 class EscapeAction(Action):
-    pass
+    def perform(self, engine, entity):
+        raise SystemExit()
 
 
 class MovementAction(Action):
     def __init__(self, dx: int, dy: int):
         super().__init__()
-
         self.dx = dx
         self.dy = dy
+
+    def perform(self, engine, entity):
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+
+        if not engine.game_map.in_bounds(dest_x, dest_y):
+            return  # Destination is out of bounds.
+        if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
+            return  # Destination is blocked by a tile.
+
+        entity.move(self.dx, self.dy)
