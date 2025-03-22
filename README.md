a9566de314441ba15f26fe84eb9ef2b5279d6428
jakobpederson <jakob.pederson+github@gmail.com>
2025-03-22 12:13:16 -0700

    add multiple rooms

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rogueliketutorials
 [python roguelike tutorial](https://www.rogueliketutorials.com/tutorials/tcod/v2/)
+
+command to run: `python main.py`
+
--- procgen.py
+++ procgen.py
@@ -24,6 +24,15 @@ class RectangularRoom:
         """Return the inner area of this room as a 2D array index."""
         return slice(self.x1 + 1, self.x2), slice(self.y1 + 1, self.y2)
 
+    def intersects(self, other):
+        """Return True if this room overlaps with another RectangularRoom."""
+        return (
+            self.x1 <= other.x2
+            and self.x2 >= other.x1
+            and self.y1 <= other.y2
+            and self.y2 >= other.y1
+        )
+
 
 def tunnel_between(start, end):
     """Return an L-shaped tunnel between these two points."""
@@ -43,12 +52,46 @@ def tunnel_between(start, end):
         yield x, y
 
 
-def generate_dungeon(map_width, map_height):
+def generate_dungeon(
+    max_rooms,
+    room_min_size,
+    room_max_size,
+    map_width,
+    map_height,
+    player,
+):
+    """Generate a new dungeon map."""
     dungeon = GameMap(map_width, map_height)
-    room_1 = RectangularRoom(x=20, y=15, width=10, height=15)
-    room_2 = RectangularRoom(x=35, y=15, width=10, height=15)
-    dungeon.tiles[room_1.inner] = tile_types.floor
-    dungeon.tiles[room_2.inner] = tile_types.floor
-    for x, y in tunnel_between(room_2.center, room_1.center):
-        dungeon.tiles[x, y] = tile_types.floor
+
+    rooms: List[RectangularRoom] = []
+
+    for r in range(max_rooms):
+        room_width = random.randint(room_min_size, room_max_size)
+        room_height = random.randint(room_min_size, room_max_size)
+
+        x = random.randint(0, dungeon.width - room_width - 1)
+        y = random.randint(0, dungeon.height - room_height - 1)
+
+        # "RectangularRoom" class makes rectangles easier to work with
+        new_room = RectangularRoom(x, y, room_width, room_height)
+
+        # Run through the other rooms and see if they intersect with this one.
+        if any(new_room.intersects(other_room) for other_room in rooms):
+            continue  # This room intersects, so go to the next attempt.
+        # If there are no intersections then the room is valid.
+
+        # Dig out this rooms inner area.
+        dungeon.tiles[new_room.inner] = tile_types.floor
+
+        if len(rooms) == 0:
+            # The first room, where the player starts.
+            player.x, player.y = new_room.center
+        else:  # All rooms after the first.
+            # Dig out a tunnel between this room and the previous one.
+            for x, y in tunnel_between(rooms[-1].center, new_room.center):
+                dungeon.tiles[x, y] = tile_types.floor
+
+        # Finally, append the new room to the list.
+        rooms.append(new_room)
+
     return dungeon
